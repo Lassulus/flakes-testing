a601ebe1bd4e1280b7b5e899928355effbca2ef9
lassulus <lassulus@lassul.us>
2022-11-10 13:28:38 +0100

    fnord: actually configure disko

--- flake.nix
+++ flake.nix
@@ -6,7 +6,11 @@
     nixosConfigurations.fnord = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        disko.nixosModules.disko
+        (disko.devices = import ./disk-config.nix) {};
+      ];
     };
     diskoConfigurations.fnord = import ./disk-config.nix;
   };

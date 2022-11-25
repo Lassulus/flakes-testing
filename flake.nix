9dfa39c97187e6ea0a19ca9a86ad65bf38285bdb
lassulus <lassulus@lassul.us>
2022-11-25 13:02:52 +0100

    refactor config, add reliablesite

--- flake.nix
+++ flake.nix
@@ -7,12 +7,39 @@
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [
-        ./configuration.nix
+        ./configs/universal_dhcp.nix
         disko.nixosModules.disko
         {
-          disko.devices = import ./disk-config.nix {};
+          disko.devices = import ./disk-config.nix {
+            lib = nixpkgs.lib;
+          };
           boot.loader.grub = {
             devices = [ "/dev/sda" ];
+            efiSupport = true;
+            efiInstallAsRemovable = true;
+          };
+        }
+      ];
+    };
+    nixosConfigurations.reliablesite = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [
+        ./configs/reliablesite.nix
+        disko.nixosModules.disko
+        {
+          disko.devices = import ./disk-config.nix {
+            lib = nixpkgs.lib;
+            disks = [
+              "/dev/nvme0n1"
+              "/dev/nvme1n1"
+            ];
+          };
+          boot.loader.grub = {
+            devices = [
+              "/dev/nvme0n1"
+              "/dev/nvme1n1"
+            ];
             efiSupport = true;
             efiInstallAsRemovable = true;
           };

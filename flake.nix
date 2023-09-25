bd37cfacbd3f97236c4753cdccda6251a9aff6c1
lassulus <git@lassul.us>
2023-09-25 22:23:35 +0200

    flake: showoff custom install iso

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
   inputs.nixos-anywhere.url = "github:numtide/nixos-anywhere";
   inputs.nixos-anywhere.inputs.nixpkgs.follows = "nixpkgs";
 
+  inputs.nixos-generators.url = "github:nix-community/nixos-generators";
+  inputs.nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+
   outputs = { self, nixpkgs, disko, nixos-anywhere, ... }@attrs: {
     packages."x86_64-linux".makeDiskImageTest = disko.lib.lib.makeDiskImage {
       nixosConfig = self.nixosConfigurations.reliablesite;
@@ -14,6 +17,22 @@
     packages."x86_64-linux".makeDiskScriptTest = disko.lib.lib.makeDiskImageScript {
       nixosConfig = self.nixosConfigurations.reliablesite;
     };
+    packages."x86_64-linux".installer-iso = let
+      installer = nixpkgs.lib.nixosSystem {
+        pkgs = self.inputs.nixpkgs.legacyPackages.x86_64-linux;
+        system = "x86_64-linux";
+        modules = [
+          self.inputs.nixos-generators.nixosModules.all-formats
+          ({ config, ... }: {
+            system.stateVersion = config.system.nixos.version;
+            services.sshd.enable = true;
+            users.users.root.openssh.authorizedKeys.keys = [
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDIb3uuMqE/xSJ7WL/XpJ6QOj4aSmh0Ga+GtmJl3CDvljGuIeGCKh7YAoqZAi051k5j6ZWowDrcWYHIOU+h0eZCesgCf+CvunlXeUz6XShVMjyZo87f2JPs2Hpb+u/ieLx4wGQvo/Zw89pOly/vqpaX9ZwyIR+U81IAVrHIhqmrTitp+2FwggtaY4FtD6WIyf1hPtrrDecX8iDhnHHuGhATr8etMLwdwQ2kIBx5BBgCoiuW7wXnLUBBVYeO3II957XP/yU82c+DjSVJtejODmRAM/3rk+B7pdF5ShRVVFyB6JJR+Qd1g8iSH+2QXLUy3NM2LN5u5p2oTjUOzoEPWZo7lykZzmIWd/5hjTW9YiHC+A8xsCxQqs87D9HK9hLA6udZ6CGkq4hG/6wFwNjSMnv30IcHZzx6IBihNGbrisrJhLxEiKWpMKYgeemhIirefXA6UxVfiwHg3gJ8BlEBsj0tl/HVARifR2y336YINEn8AsHGhwrPTBFOnBTmfA/VnP1NlWHzXCfVimP6YVvdoGCCnAwvFuJ+ZuxmZ3UzBb2TenZZOzwzV0sUzZk0D1CaSBFJUU3oZNOkDIM6z5lIZgzsyKwb38S8Vs3HYE+Dqpkfsl4yeU5ldc6DwrlVwuSIa4vVus4eWD3gDGFrx98yaqOx17pc4CC9KXk/2TjtJY5xmQ=="
+            ];
+          })
+        ];
+      };
+    in installer.config.formats.install-iso;
     nixosConfigurations.fnord = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
